3f083b036d35c7be9150c8c5cff0d11fca374195
nandhakumar2611 <unandhakumar2000@gmail.com>
2023-12-13 16:02:24 +0530

    react basic table

--- src/component/BasicTable.jsx
+++ src/component/BasicTable.jsx
@@ -34,20 +34,17 @@ const BasicTable = () => {
     const tableInst = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() })
     return (
         <>
-            <section class="py-1 bg-blueGray-50">
-                <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
-                    <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
-                        <div class="rounded-t mb-0 px-4 py-3 border-0">
-                            <div class="flex flex-wrap items-center">
-                                <div class="relative w-full px-4 max-w-full flex-grow flex-1">
-                                    <h3 class="font-semibold text-base text-blueGray-700">Basic Table</h3>
+            <section className="py-1 bg-blueGray-50">
+                <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+                    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+                        <div className="rounded-t mb-0 px-4 py-3 border-0">
+                            <div className="flex flex-wrap items-center">
+                                <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                                    <h3 className="font-semibold text-base text-blueGray-700">Basic Table</h3>
                                 </div>
-                                {/* <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-                                    <button class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
-                                </div> */}
                             </div>
                         </div>
-                        <div class="block w-full overflow-x-auto">
+                        <div className="block w-full overflow-x-auto">
                             <table className='border-collapse border'>
                                 <thead>
                                     {tableInst.getHeaderGroups().map(headerGroup => (
